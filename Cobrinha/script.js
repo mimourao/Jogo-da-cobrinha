7f3030af176ce6d32c367e304e3a4da614df5769
mimourao <a.smourao@hotmail.com>
2021-05-13 17:21:33 -0300

    criar fim de jogo

--- Cobrinha/script.js
+++ Cobrinha/script.js
@@ -32,18 +32,24 @@ function drawFood(){
 document.addEventListener("keydown", update);
 
 function update (event){
-  /*  if(event.keycode == 37 && direction != "right") direction = "left";
+    if(event.keycode == 37 && direction != "right") direction = "left";
     if(event.keycode == 38 && direction != "down") direction = "up";
     if(event.keycode == 39 && direction != "left") direction = "right";
-    if(event.keycode == 40 && direction != "up") direction = "down";*/
+    if(event.keycode == 40 && direction != "up") direction = "down";
 }
 
 function iniciarJogo(){    
-
- /* if(snak[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    /*if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
     if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
-    if(snak[0].y > 15 * box && direction == "down") snake[0].y = 0;
-    if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;  */
+    if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+    if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box; */ 
+
+    for(i = 1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo)
+            alert('Game over')
+        }
+    }
         
     criarBG();
     criarCobrinha();
@@ -57,7 +63,14 @@ function iniciarJogo(){
     if(direction == "up") snakey -= box;
     if(direction == "down") snakey += box;
 
-    snake.pop();
+    if(snakex != food.x || snakey != food.y){
+        snake.pop();
+    }
+    else{
+        food.x = Math.floor(Math.random() * 15 + 1) * box;
+        food.y = Math.floor(Math.random() * 15 + 1) * box;
+    }
+    
 
     let newHead = {
         x: snakex,
