95316a6d431ff7ce0764888871ae13adcddb9bb5
mimourao <a.smourao@hotmail.com>
2021-05-13 16:52:12 -0300

    criando food

--- Cobrinha/script.js
+++ Cobrinha/script.js
@@ -4,25 +4,50 @@ let box = 32;
 let snake = [];
 snake [0] ={
     x: 8 * box,
-    y: 8 * box,
+    y: 8 * box
 }
 let direction = "right";
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
 
-function criarBG(){
+function criarBG(){         
     context.fillStyle = "lightgreen";
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
-function criarCobrinha(){
+function criarCobrinha(){           
     for(i=0; i < snake.length; i++){
         context.fillStyle = "green";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
 
-function iniciarJogo(){
-    criarBG()
+function drawFood(){
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
+}
+
+document.addEventListener("keydown", update);
+
+function update (event){
+  /*  if(event.keycode == 37 && direction != "right") direction = "left";
+    if(event.keycode == 38 && direction != "down") direction = "up";
+    if(event.keycode == 39 && direction != "left") direction = "right";
+    if(event.keycode == 40 && direction != "up") direction = "down";*/
+}
+
+function iniciarJogo(){    
+
+ /* if(snak[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+    if(snak[0].y > 15 * box && direction == "down") snake[0].y = 0;
+    if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;  */
+        
+    criarBG();
     criarCobrinha();
+    drawFood();
 
     let snakex = snake[0].x;
     let snakey = snake[0].y;
@@ -36,7 +61,7 @@ function iniciarJogo(){
 
     let newHead = {
         x: snakex,
-        y: snakey,
+        y: snakey
     }
 
     snake.unshift(newHead);
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Jogo da cobrinha
 Este é o projeto pra ser  entregue como tarefa de javascipt
-Esta alteração foi feita remotamente
+
